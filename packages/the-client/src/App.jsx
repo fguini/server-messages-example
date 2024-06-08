362d84d8882fe2c8fdc4030f8740ff0388c7d089
Facundo <fguini@gmail.com>
2024-06-08 14:00:07 +0200

    Server events initial test

--- packages/the-client/src/App.jsx
+++ packages/the-client/src/App.jsx
@@ -1,33 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+let eventSource = null;
+
+function connect() {
+  if(!eventSource) {
+    eventSource = new EventSource('http://localhost:3000/connect');
+  }
+
+  return eventSource;
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [ messages, setMessages ] = useState([]);
+
+  useEffect( () => {
+    const events = connect();
+
+    events.onmessage = (event) => {
+      const { message, messages } = JSON.parse(event.data);
+
+      console.log('receiving messages', { message, messages });
+
+      setMessages((prevMessages) => prevMessages.concat(message ? [ message ] : messages));
+    };
+
+    return () => events.onmessage = null;
+  }, [messages]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const message = event.target.message.value;
+
+    console.log('sending a message', {
+      message
+    });
+
+    fetch('http://localhost:3000/publish', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message })
+    })
+      .then(() => event.target.reset())
+      .catch(console.error);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Server events</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <textarea readOnly rows={20} style={{ width: '20rem' }} value={ messages.join('\n') }></textarea>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={ handleSubmit }>
+        <input type="text" name="message" />
+        <button type="submit">Send</button>
+      </form>
     </>
   )
 }
--- packages/the-server/src/index.ts
+++ packages/the-server/src/index.ts
@@ -1,13 +1,64 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser'
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+let clientList: Array<{ id: number, res: Response }> = [];
+const messages: Array<string> = [];
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200
+}))
+app.use(bodyParser.json())
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({});
+});
+
+app.get('/connect', (req: Request, res: Response) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+
+  res.write(
+    `data: ${JSON.stringify({ messages })}\n\n`
+  );
+
+  const id = Date.now();
+  const newClient = {
+    id,
+    res
+  };
+
+  clientList.push(newClient);
+
+  req.on('close', () => {
+    console.log(`Client ${ id } connection closed`);
+
+    clientList = clientList.filter(client => client.id !== id);
+  });
+});
+
+app.post('/publish', (req: Request, res: Response) => {
+  console.log(req.body)
+
+  const { message } = req.body;
+
+  messages.push(message);
+
+  clientList.forEach((client) => client.res.write(
+    `data: ${JSON.stringify({ message })}\n\n`
+  ));
+
+  res.status(200).json({ messages });
 });
 
 app.listen(port, () => {
